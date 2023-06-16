b1852ab60635ce8421c43e531d40cd36e3265442
huli <2969054528@qq.com>
2023-06-16 17:10:57 +0800

    feat: 状态更新机制

--- packages/react-reconciler/src/fiber.ts
+++ packages/react-reconciler/src/fiber.ts
@@ -1,6 +1,8 @@
 import { Props, Key, Ref } from 'shared/ReactTypes';
 import { WorkTag } from './workTags';
 import { Flag, NoFlags } from './fiberFlags';
+import { Container } from 'hostConfig';
+import { UpdateQueue } from './updateQueue';
 
 export class FiberNode {
 	type: any;
@@ -16,9 +18,12 @@ export class FiberNode {
 
 	pendingProps: Props;
 	memoizedProps: Props | null;
+	memoizedState: any;
 
 	alternate: FiberNode | null;
 	flags: Flag;
+
+	updateQueue: unknown;
 
 	constructor(tag: WorkTag, pendingProps: Props, key: Key) {
 		this.tag = tag;
@@ -39,8 +44,50 @@ export class FiberNode {
 		// 作为工作单元
 		this.pendingProps = pendingProps;
 		this.memoizedProps = null;
+		this.memoizedState = null;
+		this.updateQueue = null;
 
 		this.alternate = null;
+		// 副作用
 		this.flags = NoFlags;
 	}
 }
+
+export class FiberRootNode {
+	container: Container;
+	current: FiberNode;
+	finishedWork: FiberNode | null;
+	constructor(container: Container, hostRootFiber: FiberNode) {
+		this.container = container;
+		this.current = hostRootFiber;
+		hostRootFiber.stateNode = this;
+		this.finishedWork = null;
+	}
+}
+
+export const createWorkInProgress = (
+	current: FiberNode,
+	pendingProps: Props
+): FiberNode => {
+	let wip = current.alternate;
+
+	if (wip === null) {
+		// mount
+		wip = new FiberNode(current.tag, pendingProps, current.key);
+		wip.stateNode = current.stateNode;
+
+		wip.alternate = current;
+		current.alternate = wip;
+	} else {
+		// update
+		wip.pendingProps = pendingProps;
+		wip.flags = NoFlags;
+	}
+	wip.type = current.type;
+	wip.updateQueue = current.updateQueue;
+	wip.child = current.child;
+	wip.memoizedProps = current.memoizedProps;
+	wip.memoizedState = current.memoizedState;
+
+	return wip;
+};
